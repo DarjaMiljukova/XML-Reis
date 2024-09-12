950c2ab104b0ed6a82e371cd8cfb138bc22f9a9b
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2024-09-12 12:53:34 +0300

    sql faili loomine

---
+++ reisid.sql
@@ -0,0 +1,62 @@
+create database Reis;
+use Reis;
+
+create table reisid (
+	reisID INT,
+	Aeg VARCHAR(50),
+	Alguskuupaev DATE,
+	Loppkuupaev DATE,
+	Lennujaama VARCHAR(50),
+	Riik VARCHAR(50),
+	Linn VARCHAR(50),
+	Hind VARCHAR(50)
+);
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (1, '22:37', '2023-12-30', '2024-08-26', 'Metz-Nancy-Lorraine Airport', 'Costa Rica', 'Turrialba', '€4453,25');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (2, '23:27', '2023-09-17', '2024-05-11', 'Alferez Armando Rodriguez Airport', 'China', 'Tongxing', '€7096,85');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (3, '7:02', '2024-01-21', '2024-02-08', 'Warraber Island Airport', 'China', 'Lühua', '€6497,23');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (4, '22:37', '2024-03-11', '2023-09-21', 'Bongor Airport', 'Czech Republic', 'Chlumčany', '€4476,99');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (5, '7:05', '2024-01-08', '2023-10-29', 'Inishmaan Aerodrome', 'China', 'Laixi', '€4134,82');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (6, '18:37', '2023-11-12', '2024-08-30', 'Ewo Airport', 'Libya', 'Sirte', '€8671,31');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (7, '14:55', '2023-12-18', '2024-06-24', 'Sulaco Airport', 'Argentina', 'General José de San Martín', '€8675,51');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (8, '8:11', '2024-08-09', '2024-01-28', 'Zagreb Airport', 'Azerbaijan', 'Xankandi', '€7084,23');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (9, '23:24', '2024-07-31', '2023-11-21', 'Tandag Airport', 'Philippines', 'Magtangol', '€4370,22');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (10, '11:56', '2024-08-25', '2023-12-16', 'Southern Wisconsin Regional Airport', 'Brazil', 'Pontes e Lacerda', '€3929,74');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (11, '18:07', '2024-07-27', '2024-07-12', 'Granville Airport', 'Cuba', 'Jatibonico', '€1998,19');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (12, '23:10', '2023-10-29', '2023-11-23', 'Eirunepé Airport', 'China', 'Dikou', '€9418,41');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (13, '20:15', '2024-06-20', '2024-07-18', 'Letterkenny Airport', 'Brazil', 'Sinop', '€3609,86');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (14, '2:57', '2023-09-22', '2024-01-10', 'Derby Airport', 'Poland', 'Ińsko', '€1827,44');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (15, '20:29', '2024-01-29', '2024-04-30', 'Dhangarhi Airport', 'China', 'Rinbung', '€4632,27');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (16, '19:21', '2024-03-04', '2023-10-16', 'Sesheke Airport', 'Croatia', 'Kašina', '€2172,47');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (17, '9:07', '2023-10-28', '2024-04-25', 'Ceres Airport', 'Indonesia', 'Taloko', '€7048,32');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (18, '16:44', '2024-06-30', '2024-03-17', 'Francisco Beltrão Airport', 'China', 'Wu’an', '€8821,82');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (19, '19:28', '2024-03-24', '2023-09-18', 'Tyndall Air Force Base', 'China', 'Daba', '€7037,02');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (20, '21:19', '2024-07-29', '2024-02-21', 'Carrasco International /General C L Berisso Airport', 'China', 'Pingxiang', '€3696,64');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (21, '18:42', '2024-02-26', '2024-05-26', 'Lolland Falster Maribo Airport', 'Brazil', 'Rio de Janeiro', '€1180,79');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (22, '16:06', '2023-11-23', '2024-05-26', 'Arenal Airport', 'Zimbabwe', 'Chinhoyi', '€2643,13');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (23, '15:43', '2024-02-02', '2024-08-22', 'Berlin Regional Airport', 'Poland', 'Pogorzyce', '€797,27');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (24, '4:47', '2024-04-20', '2023-12-04', 'Staniel Cay Airport', 'Indonesia', 'Bulakan', '€7926,95');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (25, '4:16', '2024-08-22', '2024-09-09', 'LBJ Ranch Airport', 'France', 'Oullins', '€8958,72');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (26, '7:11', '2024-04-10', '2023-09-12', 'Innisfail Airport', 'Madagascar', 'Ambarakaraka', '€9648,06');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (27, '23:58', '2023-10-26', '2024-05-08', 'Orchid Beach Airport', 'Ukraine', 'Boromlya', '€9192,29');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (28, '18:48', '2023-10-15', '2023-12-14', 'Erldunda Airport', 'Serbia', 'Sremska Mitrovica', '€5224,37');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (29, '9:48', '2024-08-24', '2023-10-20', 'Giyani Airport', 'China', 'Hanting', '€1452,91');
+insert into reisid (reisID, Aeg, Alguskuupaev, Loppkuupaev, Lennujaama, Riik, Linn, Hind) values (30, '16:05', '2023-10-27', '2023-11-30', 'Trent Lott International Airport', 'Indonesia', 'Ciemas', '€7173,90');
+
+
+SELECT 
+    reisID AS '@reisID',
+    Aeg AS '@Aeg',
+    Hind AS '@Hind',
+    (SELECT 
+        Alguskuupaev AS 'Alguskuupaev',
+        Loppkuupaev AS 'Loppkuupaev'
+     FOR XML PATH('Kuupaevad'), TYPE),
+    (SELECT 
+        Riik AS 'Riik',
+        Linn AS 'Linn'
+     FOR XML PATH('Sihtkoht'), TYPE),
+    (SELECT 
+        Lennujaama AS 'Lennujaam'
+     FOR XML PATH('Transport'), TYPE)
+FROM reisid
+FOR XML PATH('Reis'), ROOT('Reisid');
